2e53855e6466dc188e070e44d020acb802abb3e0
brynniebear <williamsbrynn@gmail.com>
2021-06-29 11:42:34 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary</h1>
+        <h4>Search for a word</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
