bb75280202afa5e7c1049380f72620cdece511ab
brynniebear <williamsbrynn@gmail.com>
2021-07-02 14:55:07 -0700

    Added HTML5 audio player to phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -6,8 +6,11 @@ export default function Phonetics(props) {
       {props.phoneticDetails.map(function (phonetics, index){
         return (
           <div key={index}>
-            <a href={phonetics.audio} target="_blank" rel="noreferrer">Listen</a>            
             <span className="phonetic-text">{phonetics.text}</span>
+            <figure className="phonetic-audio">
+              <audio controls src={phonetics.audio}><p>Your browser doesn't support HTML5 audio. Here is a 
+                <a href={phonetics.audio}>link to the audio</a> instead.</p></audio>          
+            </figure>  
           </div>
         );
       })}
